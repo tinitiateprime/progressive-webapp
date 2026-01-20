2ca98744f6d2e78f585ef4d42f51784df695dae1
Nikhil Ts <tsn.nikhil@gmail.com>
2026-01-20 14:48:38 +0530

    removed turbo

--- next.config.js
+++ next.config.js
@@ -1,18 +1,6 @@
-const withPWA = require("next-pwa")({
-  dest: "public",
-  register: true,
-  skipWaiting: true,
-  disable: process.env.NODE_ENV === "development",
-});
-
 /** @type {import('next').NextConfig} */
-module.exports = withPWA({
+module.exports = {
   reactStrictMode: true,
-  experimental: {
-    // Remove the turbo option if not supported in your version
-    // turbo: false
-  },
-  turbopack: {
-    root: "/home/site/wwwroot",  // Set this to the correct working directory
-  },
-});
+  // Removed Turbopack-related config
+  // Other configurations...
+};
