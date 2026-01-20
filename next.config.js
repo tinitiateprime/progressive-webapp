b430d88ac959be1044519171f1a43bc0dee45c77
Nikhil Ts <tsn.nikhil@gmail.com>
2026-01-20 10:47:38 +0530

    update changes

---
+++ next.config.js
@@ -0,0 +1,15 @@
+const withPWA = require("next-pwa")({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === "development"
+});
+
+/** @type {import('next').NextConfig} */
+module.exports = withPWA({
+  reactStrictMode: true,
+
+  experimental: {
+    turbo: false
+  }
+});
