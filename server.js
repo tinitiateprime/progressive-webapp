9ee14ce7215a55098859e6453630c9a150230766
Nikhil Ts <tsn.nikhil@gmail.com>
2026-01-20 12:19:49 +0530

    commit server and web config

---
+++ server.js
@@ -0,0 +1,21 @@
+// server.js
+/* Simple Node server for Next.js on Azure App Service (Windows) */
+
+const { createServer } = require('http');
+const next = require('next');
+
+const dev = process.env.NODE_ENV !== 'production';
+const hostname = '0.0.0.0';
+const port = process.env.PORT || 3000; // Azure will inject PORT
+
+const app = next({ dev, hostname, port });
+const handle = app.getRequestHandler();
+
+app.prepare().then(() => {
+  createServer((req, res) => {
+    handle(req, res);
+  }).listen(port, (err) => {
+    if (err) throw err;
+    console.log(`> Next.js app ready on port ${port}`);
+  });
+});
